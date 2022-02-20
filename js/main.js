2ba3ac1af0061d740305c86a8f759c16dfcc2175
Rinat Alimov <hostog@gmail.com>
2022-02-20 21:34:05 +0300

    corrected functions

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
-function getRandomNumber(minNumber, maxNumber) {
+const getRandomNumber = (minNumber, maxNumber) => {
   const MIN = Math.ceil(minNumber);
   const MAX = Math.floor(maxNumber);
 
   if (MIN < 0 || MAX < 0) {
-    return 'Числа меньше нуля';
-  } else if (MIN > MAX) {
-    return 'Не правильный диапазон цифр';
+    // eslint-disable-next-line no-console
+    console.log('Числа меньше нуля');
+    return -1;
+  }
+  if (MIN >= MAX) {
+    // eslint-disable-next-line no-console
+    console.log('Не правильный дапазон чисел');
+    return -1;
   }
 
   return Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
-}
+};
 
-getRandomNumber();
+getRandomNumber(9, 27);
 
-function stringLength(maxStringLength) {
-  return maxStringLength <= 140;
-}
+const stringLength = (maxStringLength) => maxStringLength <= 140;
 
 stringLength();
