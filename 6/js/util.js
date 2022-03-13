78233e2b00a5ddd4841c8790803df168a3757293
Keks <keks@htmlacademy.ru>
2022-03-13 19:00:48 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/util.js
@@ -0,0 +1,13 @@
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const checkStringLength = (string, length) => string <= length;
+
+export { getRandomNumber, checkStringLength };
